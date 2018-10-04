e659d684e940c3ad16af52864c40be8db2a39adf
albarajas <alejandro.barajas@loyola.edu.mx>
2018-10-03 19:28:53 -0500

    Interface Repositorio

---
+++ LoyolaMovil/DAL/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public interface IRepository<TEntity> : IDisposable where TEntity : class
+    {
+        TEntity Create(TEntity toCreate);
+
+        TEntity Retrieve();
+
+        bool Delete(TEntity toDelete);
+
+        bool Update(TEntity toUpdate);
+
+    }
+}
