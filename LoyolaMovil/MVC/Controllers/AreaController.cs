3283064de0e314be879b37167fe498c91ba233b1
alejandro.barajas <alejandro.barajas@PC-PAVILION>
2018-11-02 13:12:35 -0600

    Cambios VistaModel _ 02_11_2018

--- LoyolaMovil/MVC/Controllers/AreaController.cs
+++ LoyolaMovil/MVC/Controllers/AreaController.cs
@@ -11,13 +11,52 @@ namespace MVC.Controllers
 {
     public class AreaController : Controller
     {
-        // GET: Area
+        /// <summary>
+        /// Este metodo va a crear una lista del modelo vmListaAreas
+        /// y tengo que ir a la BD a tomar los registros y despues
+        /// construir la lista manualmente....
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             var areaBLL = new AreasBLL();
             List<tblArea> listaAreas = areaBLL.RetrieveAll();
 
-            return View(listaAreas);
+            var colBLL = new ColaboradorBLL();
+            tblColaboradore objCol;
+
+            var aulaBLL = new AulaBLL();
+            tblAula objAula;
+
+            //creo un objeto de la vm para almacenar temporalmente los registros....
+            vmListaAreas objTemp;
+
+            //creo una lista vm para almacenar los objetos....
+            List<vmListaAreas> listaFinal = new List<vmListaAreas>();
+
+            foreach(var i in listaAreas)
+            {
+                objCol = colBLL.RetrieveColaboradorByID(i.idColaborador);
+                string nombreColaborador = objCol.nombreColaborador;
+
+                objAula = aulaBLL.RetrieveAulaByID(i.idAula);
+                string nombreAula = objAula.nombreAula;
+
+                objTemp = new vmListaAreas()
+                {
+                    idArea = i.idArea,
+                    nombreArea = i.nombreArea,
+                    horaInicio = i.horaInicio.ToShortTimeString(),
+                    horaFinal = i.horaFinal.ToShortTimeString(),
+                    idColaborador = nombreColaborador,
+                    idAula = nombreAula
+                };
+
+                listaFinal.Add(objTemp);
+            }
+
+
+            return View(listaFinal);
         }
 
         public ActionResult Create()
---
+++ LoyolaMovil/MVC/Models/vmListaAreas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class vmListaAreas
+    {
+        public int idArea { get; set; }
+        public string nombreArea { get; set; }
+        public string horaInicio { get; set; }
+        public string horaFinal { get; set; }
+        public string idColaborador { get; set; }
+        public string idAula { get; set; }
+    }
+}
